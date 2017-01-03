c4ece848b0641703c87b7ed6c49b32b6e06b118f
XiaochenCui <jcnlcxc@163.com>
2017-01-04 04:55:26 +0800

    update set default editor

--- scripts/init-vim-ubuntu.sh
+++ scripts/init-vim-ubuntu.sh
@@ -32,7 +32,10 @@ make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
 sudo make install
 
 # set vim as default editor after install complete
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
 sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
 
 # for plugins tag-misc, easy-tag, tagbar
 sudo apt-get install exuberant-ctags
