623bf0c5740288f66ef2d6452b8328e311643ac5
XiaochenCui <jcnlcxc@163.com>
2017-02-03 16:00:02 +0800

    Update install-powerline-fonts.sh

--- install-powerline-fonts.sh
+++ install-powerline-fonts.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+function echoo {
+    echo -e "\n\033[32m $1 \033[0m"
+}
+
+echoo ">>> Install powerline fonts ..."
+
 wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 sudo mv PowerlineSymbols.otf /usr/share/fonts/
 sudo fc-cache -vf
